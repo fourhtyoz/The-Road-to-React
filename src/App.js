dd346133353f3bef0085d438ceff49e0b3d723ea
fourhtyoz <fourhtyoz@gmail.com>
2022-11-24 13:32:14 +0300

    state

--- src/App.js
+++ src/App.js
@@ -1,81 +1,61 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    point: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
+//   const a = [1, 2, 3, 4, 5, 6]
+//   const b = ['a', 'b', 'c', 'd', 'e', 'f']
+
+const [searchTerm, setSearchTerm] = useState('');
+const handleSearch = e => {
+    setSearchTerm(e.target.value);
 }
-
-function getName(name) {
-  return name;
-}
-
-const a = ['a', 'b', 'c'];
-const b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
-function App() {
-  return (
+const searchedStories = stories.filter(story => 
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
-      <h2>{getName('Boris')}</h2>
-      <p>{a.map((x) => <h3>{x}</h3>)}</p>
       <h1>My Hacker Stories</h1>
-      <label htmlFor='search'>Search: </label>
-      <input id="search" type="text" />
-      <hr/>
-      {list.map(function(item) {
-          return <div>{item.title}</div>
-      })}
-      <hr/>
-      {list.map(function(item) {
-        return (
-          <div key={item.objectID}>
-            {item.title}
-          </div>
-        );
-      })}
-      <hr/>
-      {list.map(function(item) {
-        return (
-          <div key={item.objectID}>
-            <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.point}</span>
-          </div>
-        )
-      })}
-      <hr/>
-      {b.reduce(function(previous, current) {
-          return (
-            <div>
-              <span>{previous + current}</span>
-            </div>
-          )
-      }
-      )}
+      <Search onSearch={handleSearch} />
+      <hr />
+      <List list={searchedStories} />
     </div>
   );
-}
+};
+
+const Search = props => (
+    <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={props.onSearch} />
+    </div>
+);
+        
+const List = props =>
+    props.list.map(item => (
+    <div key={item.objectID}>
+        <span><a href={item.url}>{item.title}</a></span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+    </div>
+));
 
 export default App;
